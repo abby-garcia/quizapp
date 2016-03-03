d3a3d4efcb4ca1caf85b1ada5b42499ab556e99b
abby-garcia <acg361@gmail.com>
2016-03-02 22:40:14 -0500

    more jq

--- js/main.js
+++ js/main.js
@@ -2,11 +2,31 @@
 $( document ).ready(function() {
     
 	$('.musicinstrumentquiz').click(function(){
-		$('#musical-instrument-quiz > .question1').show();
+		$('#musical-instrument-quiz').show();
+		$('.instrument-question1').show();
+		$('.instrument-question2').hide();
+		$('.instrument-question3').hide();
+		$('.instrument-question4').hide();
+		$('.instrument-question5').hide();
+		$('.boxes').hide();
+		$('.social').hide();
+
+	});
+
+	//below: when they click on any button in question 1, 
+	// they will be brought to the next question
+
+	$('instrument-question1').click(function(){
+		$('')
+
 	});
 
 
-	
+
+
+	$
+
+
 
 
 
