8587fb435d639648119b748a5c26659ded09cb59
abby-garcia <acg361@gmail.com>
2016-03-03 23:13:40 -0500

    jquery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 
 $( document ).ready(function() {
+
+	$('#new-game').click(function(){ //this isn't work :( why?
+		$('#first-page').show();
+		$('.instrument-question1').hide();
+		$('.instrument-question2').hide();
+		$('.instrument-question3').hide();
+		$('.instrument-question4').hide();
+		$('.instrument-question5').hide();
+	});
     
 	$('.musicinstrumentquiz').click(function(){
 		$('#musical-instrument-quiz').show();
@@ -16,15 +25,40 @@ $( document ).ready(function() {
 	//below: when they click on any button in question 1, 
 	// they will be brought to the next question
 
-	$('instrument-question1').click(function(){
-		$('')
+	$('.instrument-question1').click(function(){
+		$('.instrument-question2').show();
+		$('.instrument-question1').hide();
+
+	});
+
+	$('.instrument-question2').click(function(){
+		$('.instrument-question3').show();
+		$('.instrument-question2').hide();
+
+	});
+
+	$('.instrument-question3').click(function(){
+		$('.instrument-question4').show();
+		$('.instrument-question3').hide();
+
+	});
+
+	$('.instrument-question4').click(function(){
+		$('.instrument-question5').show();
+		$('.instrument-question4').hide();
+
+	});
+
+	$('.instrument-question5').click(function(){
+		$('.instrument-question5').hide();
+		// $('#musical-instrument-quiz).hide();
+		$('.complete-boxed').show();
 
 	});
 
 
 
 
-	$
 
 
 
